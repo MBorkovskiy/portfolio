39978505694abf965600acd9e2b56a113c643d50
Mborkovskiy <borkovskiy.m@gmail.com>
2023-06-01 17:37:45 +0300

    update

--- src/Components/FirstScreen/FirstScreen.jsx
+++ src/Components/FirstScreen/FirstScreen.jsx
@@ -16,6 +16,7 @@ export const FirstScreen = () => {
       component={motion.div}
       initial="hidden"
       whileInView="visible"
+      viewport={{ amount: 0.2 }}
       direction={{ xs: "column-reverse", md: "row" }}
       padding={{ xs: "50px 15px", sm: "100px", md: "150px" }}
       spacing={{ xs: 2, sm: 5, md: 10 }}

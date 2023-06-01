dc6f850efcd9018050fa41eca12d337e63c81e2c
Mborkovskiy <borkovskiy.m@gmail.com>
2023-06-01 14:52:17 +0300

    update

--- src/Components/About/About.jsx
+++ src/Components/About/About.jsx
@@ -33,13 +33,13 @@ export const About = () => {
             fontSize={{ xs: "14px", sm: "16px" }}
           >
             As a Front-End Developer, I possess an impressive arsenal of skills
-            in HTML, CSS, SCSS, JavaScript, React, Tailwind, and MUI. I excel in
-            designing and maintaining responsive websites that offer a smooth
-            user experience. My expertise lies in crafting dynamic, engaging
-            interfaces through writing clean and optimized code and utilizing
-            cutting-edge development tools and techniques. I am also a team
-            player who thrives in collaborating with cross-functional teams to
-            produce outstanding web applications.
+            in HTML, CSS, SCSS, JavaScript, React, Redux Toolkit, Tailwind, and
+            MUI. I excel in designing and maintaining responsive websites that
+            offer a smooth user experience. My expertise lies in crafting
+            dynamic, engaging interfaces through writing clean and optimized
+            code and utilizing cutting-edge development tools and techniques. I
+            am also a team player who thrives in collaborating with
+            cross-functional teams to produce outstanding web applications.
           </Typography>
         </Stack>
       </Stack>
